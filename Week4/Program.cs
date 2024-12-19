0ac20c1732611b7a3f8f61e633874fe59531619b
Roosikas <jan.roos@tktk.ee>
2024-12-19 22:53:49 +0200

    add week4 code

---
+++ Week4/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//For-Loop
+//DRY- principle- Do Not Repeat Yourself
+
+int i = 0;
+
+for(i = 0; i < 5; i++) //I++=i+i
+{
+    Console.WriteLine("We dont need no education!");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day!");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true;
+
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    //Console.WriteLine($" cpu has generated {cpuRandom}.");
+    Console.WriteLine("Make a quess. Enter a number1-3");
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congratulation, You won!");
+        loopActive = false;
+    }
+    else
+    {
+
+        Console.WriteLine("Oops. Try agan!");
+
+    }
+}
+Console.WriteLine("Have a nice day!");
